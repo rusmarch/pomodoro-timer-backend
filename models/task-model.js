f4bb417d88a7b547f1a31d1ab2ca5fa97e6d2c82
rusmarch <cheustor@gmail.com>
2024-02-28 14:18:17 +0200

    initial

---
+++ models/task-model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const TaskSchema = new Schema({
+   user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+   },
+   title: {
+      type: String,
+      required: true
+   },
+   complete: {
+      type: Boolean,
+      default: false
+   },
+   totalTime: {
+      type: Number,
+      default: 0
+   },
+   tags: {
+      type: [String],
+      default: [],
+   },
+})
+
+module.exports = model('Task', TaskSchema);
+
